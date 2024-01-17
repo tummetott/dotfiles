2c13300c43f4ba297f1991168737ec3d5b66e2d5
Tummetott <19708025+Tummetott@users.noreply.github.com>
2024-01-17 23:05:33 +0100

    gitsigns: keymaps are always present and not attached when the plugin loads

---
+++ dot_config/nvim/lua/tummetott/plugins/gitsigns.lua
@@ -0,0 +1,119 @@
+return {
+    'lewis6991/gitsigns.nvim',
+    enabled = true,
+    event = 'LazyFile',
+    opts = {
+        signcolumn = true,
+        signs = {
+            add = {
+                text = '│'
+            },
+            change = {
+                text = '│'
+            },
+            untracked = {
+                text = '│'
+            }
+        },
+        preview_config = {
+            border = 'rounded',
+        },
+        -- Untracked files are not shown by gitsigns. You need to manually add a
+        -- file with 'git add' before gitsigns works
+        attach_to_untracked = false,
+        trouble = true,
+    },
+    init = function()
+        require('tummetott.utils').which_key_register {
+            ['<Leader>g'] = { name = 'Gitsigns' }
+        }
+    end,
+    keys = {
+        {
+            ']h',
+            function()
+               vim.schedule(function() require 'gitsigns'.next_hunk() end)
+                return '<Ignore>'
+            end,
+            desc = 'Go to next hunk',
+            expr = true,
+        },
+        {
+            '[h',
+            function()
+                vim.schedule(function() require 'gitsigns'.prev_hunk() end)
+                return '<Ignore>'
+            end,
+            desc = 'Go to previous hunk',
+            expr = true,
+        },
+        {
+            '<Leader>gs',
+            function() require 'gitsigns'.stage_hunk() end,
+            desc = 'stage hunk',
+        },
+        {
+            '<Leader>gS',
+            function() require 'gitsigns'.stage_buffer() end,
+            desc = 'Stage buffer'
+        },
+        {
+            '<Leader>gu',
+            function() require 'gitsigns'.undo_stage_hunk() end,
+            desc = 'undo stage hunk'
+        },
+        {
+            '<Leader>gr',
+            function() require 'gitsigns'.reset_hunk() end,
+            desc = 'reset hunk'
+        },
+        {
+            '<Leader>gR',
+            function() require 'gitsigns'.reset_buffer() end,
+            desc = 'reset buffer'
+        },
+        {
+            '<Leader>gp',
+            function() require 'gitsigns'.preview_hunk() end,
+            desc = 'preview hunk'
+        },
+        {
+            '<Leader>gb',
+            function() require 'gitsigns'.blame_line() end,
+            desc = 'blame current line'
+        },
+        {
+            '<Leader>gl',
+            function() require 'gitsigns'.toggle_current_line_blame() end,
+            desc = 'toggle current line blame'
+        },
+        {
+            '<Leader>gv',
+            function() require 'gitsigns'.toggle_deleted() end,
+            desc = 'toggle virtual text diff'
+        },
+        {
+            '<Leader>gs',
+            ':Gitsigns stage_hunk<CR>',
+            mode = 'v',
+            desc = 'stage selected',
+        },
+        {
+            '<Leader>gr',
+            '<Cmd>Gitsigns reset_hunk<CR>',
+            mode = 'v',
+            desc = 'reset selected',
+        },
+        {
+            'yog',
+            function() require 'gitsigns'.toggle_signs() end,
+            desc = 'Toggle gitsigns'
+        },
+        {
+            'ih',
+            ':<C-u>Gitsigns select_hunk<CR>',
+            mode = { 'o', 'x' },
+            desc = 'inner hunk',
+        }
+    }
+}
