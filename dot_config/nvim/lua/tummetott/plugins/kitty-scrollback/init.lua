e1cd0d53700049bf0540adbf459f52fdb2b23232
Tummetott <19708025+Tummetott@users.noreply.github.com>
2024-01-22 21:34:14 +0100

    delete symlinks for kitty-scollback because kitty supports env variable expansion

--- dot_config/nvim/lua/tummetott/plugins/kitty-scrollback/init.lua
+++ dot_config/nvim/lua/tummetott/plugins/kitty-scrollback/init.lua
@@ -1,12 +1,6 @@
 return {
     'mikesmithgh/kitty-scrollback.nvim',
     enabled = true,
-    build = function()
-        local source = vim.fn.stdpath('data') .. '/lazy/kitty-scrollback.nvim'
-        local target = vim.env.XDG_CONFIG_HOME .. '/kitty'
-        local command = string.format("ln -s '%s' '%s'", source, target)
-        os.execute(command)
-    end,
     cmd = {
         'KittyScrollbackGenerateKittens',
         'KittyScrollbackCheckHealth',
