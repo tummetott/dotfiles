9e4be95ddc661fa646b7ea777dc9ada3ec1c1b05
jonas <jonas@srlabs.de>
2026-01-30 15:51:43 +0100

    jump to sidekick file/line references

--- dot_config/nvim/lua/tummetott/plugins/sidekick.lua
+++ dot_config/nvim/lua/tummetott/plugins/sidekick.lua
@@ -16,6 +16,67 @@ return {
                 require('sidekick.nes').update()
             end)
         end, 1000)
+
+        -- Find a non-terminal window in the current tabpage (your code window)
+        local function find_window_nr()
+            for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+                local buf = vim.api.nvim_win_get_buf(win)
+                if vim.bo[buf].buftype == "" then
+                    return win
+                end
+            end
+        end
+
+        local function parse_location(buf)
+            local row = vim.api.nvim_win_get_cursor(0)[1] -- 1-based
+            local total = vim.api.nvim_buf_line_count(buf)
+
+            -- current line + next two lines (because of possible line wrap)
+            local stop = math.min(row + 2, total)
+            local lines = vim.api.nvim_buf_get_lines(buf, row - 1, stop, false)
+
+            -- Cut of the first two characters of wrapped lines. they are always
+            -- padding
+            for i = 2, #lines do
+                lines[i] = lines[i]:sub(3)
+            end
+
+            local text = table.concat(lines, "")
+            local path, lnum = text:match("([%w%._%-%/]+):(%d+)")
+            if not path or not lnum then
+                return nil
+            end
+
+            return path, tonumber(lnum)
+        end
+
+        local function jump_to_location(buf)
+            local path, lnum = parse_location(buf)
+            local jump = path
+                and lnum
+                and vim.fn.filereadable(path) == 1
+                and find_window_nr()
+
+            if not jump then
+                return vim.cmd("normal! <C-]>")
+            end
+
+            vim.api.nvim_set_current_win(jump)
+            vim.cmd("edit " .. vim.fn.fnameescape(path))
+            vim.api.nvim_win_set_cursor(jump, { lnum, 0 })
+            vim.cmd("normal! zz")
+        end
+
+        vim.api.nvim_create_autocmd("TermOpen", {
+            callback = function(ctx)
+                vim.keymap.set(
+                    "n",
+                    "<C-]>",
+                    function() jump_to_location(ctx.buf) end,
+                    { buffer = ctx.buf }
+                )
+            end,
+        })
     end,
     opts = {
         nes = {
