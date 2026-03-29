1850d75ff37ff9ba53a3bdb43cb46839de41c4eb
jonas <jonas@srlabs.de>
2026-03-29 17:46:27 +0200

    fix: surround v3 to v4

--- dot_config/nvim/lua/tummetott/plugins/surround.lua
+++ dot_config/nvim/lua/tummetott/plugins/surround.lua
@@ -3,19 +3,6 @@ return {
     enabled = true,
     opts = {
         move_cursor = false,
-        keymaps = {
-            insert = false,
-            insert_line = false,
-            normal = false,
-            normal_cur = false,
-            normal_line = false,
-            normal_cur_line = false,
-            visual = false,
-            visual_line = false,
-            delete = false,
-            change = false,
-            change_line = false,
-        }
     },
     keys = {
         {
