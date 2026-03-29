74b86903637ea6e89fffd6ee8739c2dda7dd6e2c
jonas <jonas@srlabs.de>
2026-03-29 23:22:30 +0200

    fix: added keymaps for fterm

--- dot_config/nvim/lua/tummetott/plugins/fterm.lua
+++ dot_config/nvim/lua/tummetott/plugins/fterm.lua
@@ -6,29 +6,16 @@ return {
         dimensions = { height = 0.9, width = 0.9, },
     },
     keys = {
-        -- {
-        --     '<c-s>',
-        --     function() require('FTerm').toggle() end,
-        --     desc = 'Open terminal',
-        -- },
-        -- {
-        --     '<c-s>',
-        --     '<c-\\><c-n><cmd>lua require("FTerm").toggle()<cr>',
-        --     mode = 't',
-        --     desc = 'Close terminal',
-        -- },
         {
-            '<c-;>',
-            '<c-\\><c-n><c-;>i',
-            mode = 't',
-            remap = true,
-            desc = 'Toggle terminal size',
+            '<c-cr>',
+            function() require('FTerm').toggle() end,
+            desc = 'Open terminal',
         },
         {
-            '<c-k>',
-            '<c-\\><c-n><c-w>k',
+            '<c-cr>',
+            '<c-\\><c-n><cmd>lua require("FTerm").toggle()<cr>',
             mode = 't',
-            desc = 'Move to top pane',
+            desc = 'Close terminal',
         },
     }
 }
