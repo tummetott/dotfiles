25ed1081b0b691416aa8c1fca8491dc31eff1e67
Tummetott <19708025+Tummetott@users.noreply.github.com>
2026-05-07 17:12:24 +0200

    cleanup: remove sidekicks nes keymaps

--- dot_config/nvim/lua/tummetott/plugins/sidekick.lua
+++ dot_config/nvim/lua/tummetott/plugins/sidekick.lua
@@ -7,38 +7,6 @@ return {
             { '<Leader>j', group = 'Sidekick' }
         }
     end,
-    config = function(_, opts)
-        require('sidekick').setup(opts)
-
-        -- Send one NES update shortly after startup.
-        -- Reason: Copilot's Next Edit Suggestions (NES) won't respond to the
-        -- very first manual update call unless the LSP has already received a
-        -- valid text synchronization event. This deferred update "warms up"
-        -- Copilot so the first <C-,> trigger works without requiring a manual
-        -- edit beforehand.
-        vim.defer_fn(function()
-            pcall(function()
-                require('sidekick.nes').update()
-            end)
-        end, 1000)
-
-        -- Codex hardcodes <C-n>/<C-p> to navigate between questions in plan
-        -- mode. These mappings repurpose them for list navigation instead.
-        -- ISSUE: https://github.com/openai/codex/issues/3049
-        -- vim.api.nvim_create_autocmd('FileType', {
-        --     pattern = 'sidekick_terminal',
-        --     callback = function(args)
-        --         vim.keymap.set('t', "<C-n>", "<Down>", {
-        --             buffer = args.buf,
-        --             desc = 'Next Item',
-        --         })
-        --         vim.keymap.set('t', "<C-p>", "<Up>", {
-        --             buffer = args.buf,
-        --             desc = 'Previous Item',
-        --         })
-        --     end,
-        -- })
-    end,
     opts = {
         nes = {
             enabled = true,
@@ -83,71 +51,7 @@ return {
         },
     },
     keys = {
-        {
-            '<c-,>',
-            function()
-                -- Close the blink window if a copilot suggestion is fetched
-                local ok, blink = pcall(require, 'blink.cmp')
-                if ok and blink.is_menu_visible() then
-                    blink.cancel()
-                end
-                require('sidekick.nes').update()
-            end,
-            desc = 'Get a copilot suggestion',
-            mode = { 'n', 'i' },
-        },
-        {
-            '<cr>',
-            function()
-                if require('sidekick.nes').have() then
-                    require('sidekick.nes').apply()
-                else
-                    return '<cr>'
-                end
-            end,
-            expr = true,
-            mode = { 'n' }, -- Insert mode mapping in blink.nvim
-            desc = 'Apply Copilot Suggestion'
-        },
-        {
-            '<c-y>',
-            function()
-                if require('sidekick.nes').have() then
-                    require('sidekick.nes').apply()
-                else
-                    return '<c-y>'
-                end
-            end,
-            expr = true,
-            mode = { 'n', 'i' },
-            desc = 'Apply Copilot Suggestion'
-        },
-        {
-            '<c-f>',
-            function()
-                if require('sidekick.nes').have() then
-                    require('sidekick.nes').apply()
-                else
-                    vim.api.nvim_input('<Right>')
-                end
-            end,
-            mode = 'i',
-            desc = 'Apply Suggestion or Move Right'
-        },
-        {
-            '<c-e>',
-            function()
-                if require('sidekick.nes').have() then
-                    require('sidekick.nes').clear()
-                else
-                    return '<c-e>'
-                end
-            end,
-            expr = true,
-            mode = 'n', -- Insert mode mapping in blink.cmp
-            desc = 'Clear Copilot Suggestion'
-        },
-        -- HACK: workaround for this issue: https://github.com/folke/sidekick.nvim/issues/318
+        -- HACK: workaround for https://github.com/folke/sidekick.nvim/issues/318
         {
             '<c-.>',
             function()
