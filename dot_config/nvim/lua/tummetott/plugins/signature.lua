59a2b7e57fe7234cfd5c4137cf02a47dc8d02080
Tummetott <19708025+Tummetott@users.noreply.github.com>
2026-09-25 18:07:08 +0200

    fix: signature help was broken

--- dot_config/nvim/lua/tummetott/plugins/signature.lua
+++ dot_config/nvim/lua/tummetott/plugins/signature.lua
@@ -20,17 +20,5 @@ return {
             close_signature = '<c-e>'
         },
         display_automatically = true
-    },
-    config = function(_, opts)
-        vim.api.nvim_create_autocmd('LspAttach', {
-            callback = function(args)
-                local client = vim.lsp.get_client_by_id(args.data.client_id)
-                if not client then return end
-
-                if client.server_capabilities.signatureHelpProvider then
-                    require('lsp-overloads').setup(client, opts)
-                end
-            end,
-        })
-    end,
+    }
 }
