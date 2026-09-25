29da611c72aa3b31799b6e36afaead12b8cff48c
Tummetott <19708025+Tummetott@users.noreply.github.com>
2026-09-25 17:28:38 +0200

    feat: lazyload blink.pairs

--- dot_config/nvim/lua/tummetott/plugins/blink-pairs.lua
+++ dot_config/nvim/lua/tummetott/plugins/blink-pairs.lua
@@ -1,6 +1,7 @@
 return {
     'saghen/blink.pairs',
     enabled = true,
+    event = 'VeryLazy',
     version = '*', -- required with prebuilt binaries
     -- download prebuilt binaries from github releases
     dependencies = 'saghen/blink.lib',
